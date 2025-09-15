329cccc38bb922f1d8d5ea3cdd259120d7e5117a
gksdud1109 <vectorh532@gmail.com>
2025-09-15 17:08:45 +0900

    init

---
+++ src/main/java/com/jump2springboot2/Jump2SpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.jump2springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jump2SpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jump2SpringBoot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jump2springboot2/Jump2SpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jump2springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jump2SpringBoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
