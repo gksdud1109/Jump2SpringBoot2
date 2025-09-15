5aa492ced05a2ae99267a28ea2aace9e9624dfc2
gksdud1109 <vectorh532@gmail.com>
2025-09-15 23:50:01 +0900

    refactor(AnswerRepository): wrong generic fixed

--- src/main/java/com/jump2springboot2/answer/repository/AnswerRepository.java
+++ src/main/java/com/jump2springboot2/answer/repository/AnswerRepository.java
@@ -1,7 +1,7 @@
 package com.jump2springboot2.answer.repository;
 
-import com.jump2springboot2.question.entity.Question;
+import com.jump2springboot2.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository<Question, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
