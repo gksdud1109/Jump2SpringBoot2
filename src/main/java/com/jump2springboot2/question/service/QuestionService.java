34ef42de9c78567376abda48355073680ade9a90
gksdud1109 <vectorh532@gmail.com>
2025-09-15 23:24:14 +0900

    2-10

--- src/main/java/com/jump2springboot2/question/service/QuestionService.java
+++ src/main/java/com/jump2springboot2/question/service/QuestionService.java
@@ -1,6 +1,7 @@
 package com.jump2springboot2.question.service;
 
 import com.jump2springboot2.question.entity.Question;
+import com.jump2springboot2.question.exception.DataNotFoundException;
 import com.jump2springboot2.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,9 @@ public class QuestionService {
 
     public List<Question> getList(){
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Long id) {
+        return questionRepository.findById(id).orElseThrow(()-> new DataNotFoundException("질문이 없습니다."));
     }
 }
