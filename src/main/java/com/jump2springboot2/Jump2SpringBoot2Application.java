e14a00b8d150084ebfd575f443dffb7da4398a12
gksdud1109 <vectorh532@gmail.com>
2025-09-15 17:21:23 +0900

    J2SB-2-02 / refactor(build.gradle): rm no need dependency - spring security

--- src/main/java/com/jump2springboot2/Jump2SpringBoot2Application.java
+++ src/main/java/com/jump2springboot2/Jump2SpringBoot2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Jump2SpringBoot2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Jump2SpringBoot2Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Jump2SpringBoot2Application.class, args);
+	}
 
 }
